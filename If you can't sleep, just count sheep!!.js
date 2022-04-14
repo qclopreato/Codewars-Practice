b129041fbd5222925aad45a7f77c4b308bc1c053
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-14 18:44:39 -0500

    Create If you can't sleep, just count sheep!!.js

---
+++ If you can't sleep, just count sheep!!.js
@@ -0,0 +1,15 @@
+/*Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.*/
+
+var countSheep = function (num){
+  let value = "";
+  if (num === 0){
+    return value;
+  } else if(num !== 0){
+    for(let i=1;i<num+1;i++){
+      value += (i + " sheep...");
+      console.log(value);
+    }
+  return value;
+  }
+}
