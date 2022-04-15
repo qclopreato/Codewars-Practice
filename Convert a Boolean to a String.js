c3294f5eeb03c424a4a135d09854bdb49fbcdffb
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-14 19:06:29 -0500

    Create Convert a Boolean to a String.js

---
+++ Convert a Boolean to a String.js
@@ -0,0 +1,15 @@
+/*Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+  });
+});*/
+
+function booleanToString(b){
+  const output = b === true ? "true" : "false";
+  return output;
+}
