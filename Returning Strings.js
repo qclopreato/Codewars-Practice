8eb096e689d4494b96abce9719dc9cf08f5e9886
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-16 10:44:51 -0500

    Create Returning Strings.js

---
+++ Returning Strings.js
@@ -0,0 +1,18 @@
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+    assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+  })
+})*/
+
+function greet(name){
+  var myArray = [name];
+  const map1 = myArray.map( x => "Hello, " + name + " how are you doing today?");
+  return map1[0];
+}
