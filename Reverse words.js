fba2e447d93a851d868c3815776af162c8ce1b20
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-19 18:01:41 -0500

    Create Reverse words.js

---
+++ Reverse words.js
@@ -0,0 +1,23 @@
+/*Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+
+Sample Tests:
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});*/
+
+function reverseWords(str) {
+  return str.split("").reverse().join("").split(" ").reverse().join(" "); 
+}
