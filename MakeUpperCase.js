84d206f2c4f3047d7c72ab7f06c452d294c934d9
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-20 06:31:55 -0500

    Create MakeUpperCase.js

---
+++ MakeUpperCase.js
@@ -0,0 +1,12 @@
+/*Write a function which converts the input string to uppercase.
+
+Sample Tests:
+describe("Basic Tests", function(){
+  it("should pass the basic tests", function(){
+    Test.assertEquals(makeUpperCase("hello"), "HELLO");
+  });
+});*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
