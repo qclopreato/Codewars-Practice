8d2d285a7e83a5742c52da94fa4db8c3c7d0e0b8
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-20 19:39:55 -0500

    Create Convert a string to an array.js

---
+++ Convert a string to an array.js
@@ -0,0 +1,20 @@
+/*Write a function to split a string and convert it into an array of words.
+
+Examples (Input -> Output):
+* "Robin Singh" ==> ["Robin", "Singh"]
+
+Sample Tests:
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+    assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
+  });
+});*/
+
+function stringToArray(string){
+  return string.split(" ");
+}
