c3dd850d190998b64e6ae172dc2485f73d5f4ee7
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-25 20:22:00 -0500

    Create Mumbling.js

---
+++ Mumbling.js
@@ -0,0 +1,32 @@
+/*This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+Sample Tests:
+describe("accum",function() {
+it("Basic tests",function() {    
+	Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+	Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+	Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+	Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+	Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+})})*/
+
+function accum(s) {
+  const thing = s.split("");
+  const myArray = [];
+  for (let i=0;i<thing.length;i++){
+    if (i===0){
+      let first = thing.slice(i,i+1);
+      myArray.push(first.map(x => x.toUpperCase()));
+    } else {
+      let second = thing.slice(i,i+1);
+      myArray.push(second.map(x => x.toUpperCase()+ x.toLowerCase().repeat(i)));
+    } 
+  }
+  return myArray.join('-');
+}
